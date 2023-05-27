2d59266da0b91b6531b8f213a6c2213826416399
itsmo1031 <itsmo1031@gmail.com>
2023-05-27 23:51:16 +0900

    ✨ feat: add sounds

--- js/app.js
+++ js/app.js
@@ -53,6 +53,16 @@ let dropWordTimeout;
 let dropDelay;
 // 음성 인식 결과를 저장할 배열
 const speechWords = [];
+let muted = localStorage.getItem('muted')
+  ? JSON.parse(localStorage.getItem('muted'))
+  : true;
+const volumeBtn = document.getElementById('volume');
+const bgm = new Audio('static/game-bgm.mp3');
+const startMusic = new Audio('static/game-start.mp3');
+const endMusic = new Audio('static/game-end.mp3');
+const selectSound = new Audio('static/select.mp3');
+const inputSound = new Audio('static/input.mp3');
+const audios = [bgm, startMusic, endMusic, selectSound, inputSound];
 
 // result 이벤트 핸들러 설정
 recognition.onresult = (event) => {
@@ -116,6 +126,7 @@ const handleInput = (event) => {
     for (let i = 0; i < wordElements.length; i++) {
       const wordElement = wordElements[i];
       if (wordElement.innerText === inputValue.replace(' ', '')) {
+        inputSound.play();
         score += wordElement.innerText.length * settings.SCORE_PER_LETTER;
         displayScore(score);
         wordElement.parentNode.removeChild(wordElement);
@@ -154,7 +165,9 @@ const gameOver = () => {
   toggleScreen('header-game-left', 'header-retry-left');
   window.addEventListener('keydown', handleRadio);
   window.addEventListener('keyup', handleSelection);
-
+  endMusic.play();
+  bgm.pause();
+  bgm.currentTime = 0;
   finalScore.innerText = score.toString().padStart(6, '0');
   // 현재 점수 최종 점수와 비교
   if (isBestScore()) {
@@ -169,18 +182,6 @@ const gameOver = () => {
 
 const isBestScore = () => {
   return !bestScore || score > parseInt(bestScore);
-};
-
-const init = () => {
-  console.log('Initiated!');
-  window.addEventListener('keyup', handleGameStart);
-  document.querySelectorAll('#selection label').forEach((e) => {
-    e.addEventListener('mouseenter', handleRadioHover);
-    e.addEventListener('click', handleRadioClick);
-    e.childNodes.forEach((n) => {
-      n.addEventListener('click', preventHandler);
-    });
-  });
 };
 
 const displayScore = (num) => {
@@ -223,9 +224,6 @@ const stopRecognition = () => {
 
 const gameStart = () => {
   const inputDiv = document.getElementById('input-div');
-  displayBestScore();
-  clearScore();
-  startRecognition();
   inputDiv.classList.toggle('display-none');
   inputDiv.classList.toggle('input-animation');
   const inputField = document.getElementById('input-field');
@@ -235,6 +233,15 @@ const gameStart = () => {
     dropping = setInterval(handleDropWords, 1000);
   });
   inputField.addEventListener('keyup', handleInput);
+  displayBestScore();
+  clearScore();
+  startRecognition();
+  startMusic.play().then(() => {
+    setTimeout(() => {
+      bgm.loop = true;
+      bgm.play();
+    }, 2500);
+  });
 };
 
 const toggleScreen = (fromScreen, toScreen) => {
@@ -244,19 +251,38 @@ const toggleScreen = (fromScreen, toScreen) => {
   ts.classList.toggle('display-none');
 };
 
+const playSelectSound = () => {
+  selectSound.pause();
+  selectSound.currentTime = 0;
+  selectSound.play();
+};
+
 const handleRadio = (event) => {
   const radios = Array.from(document.getElementsByClassName('retry-selection'));
   const currentIndex = radios.findIndex((r) => r.checked);
   let nextIndex;
-
   if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
     nextIndex = currentIndex > 0 ? currentIndex - 1 : radios.length - 1;
+    playSelectSound();
   } else if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
     nextIndex = currentIndex < radios.length - 1 ? currentIndex + 1 : 0;
+    playSelectSound();
   }
   if (nextIndex !== undefined) {
     radios[currentIndex].checked = false;
     radios[nextIndex].checked = true;
+  }
+};
+
+const handleRadioHover = (event) => {
+  const target = event.target.firstElementChild;
+  playSelectSound();
+  if (!target.checked) {
+    const other = document.querySelector(
+      'input[name="selection-radio"]:checked'
+    );
+    other.checked = false;
+    target.checked = true;
   }
 };
 
@@ -277,23 +303,13 @@ const handleSelection = (event) => {
     } else {
       toggleScreen('retry-section', 'title-section');
       toggleScreen('header-retry-left', 'header-main-left');
+      startMusic.play();
       window.addEventListener('keyup', handleGameStart);
     }
 
     finalScore.parentNode.classList.remove('best');
     window.removeEventListener('keydown', handleRadio);
     window.removeEventListener('keyup', handleSelection);
-  }
-};
-
-const handleRadioHover = (event) => {
-  const target = event.target.firstElementChild;
-  if (!target.checked) {
-    const other = document.querySelector(
-      'input[name="selection-radio"]:checked'
-    );
-    other.checked = false;
-    target.checked = true;
   }
 };
 
@@ -304,6 +320,38 @@ const handleRadioClick = (event) => {
 
 const preventHandler = (event) => {
   if (event.defaultPrevented) return;
+};
+
+const handleClickVolume = (event) => {
+  toggleVolumeIcon();
+  muted = !muted;
+  localStorage.setItem('muted', muted);
+  console.log('muted: ' + muted);
+  audios.forEach((audio) => {
+    audio.muted = muted;
+  });
+};
+
+const toggleVolumeIcon = () => {
+  volumeBtn.classList.toggle('fa-volume-high');
+  volumeBtn.classList.toggle('fa-volume-xmark');
+};
+
+const init = () => {
+  console.log('Initiated!');
+  if (!muted) {
+    console.log('music not muted. toggle volume icon');
+    toggleVolumeIcon(volumeBtn);
+  }
+  volumeBtn.addEventListener('click', handleClickVolume);
+  window.addEventListener('keyup', handleGameStart);
+  document.querySelectorAll('#selection label').forEach((e) => {
+    e.addEventListener('mouseenter', handleRadioHover);
+    e.addEventListener('click', handleRadioClick);
+    e.childNodes.forEach((n) => {
+      n.addEventListener('click', preventHandler);
+    });
+  });
 };
 
 window.onload = init;
